c54518a71e397de4c855df75cb2d579111bdf3f9
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-13 17:52:13 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,7 +14,7 @@ class Scraper
         course.title = post.css("h2").text
         course.schedule = post.css(".date").text
         course.description = post.css("p").text
-        ruby lib/scraper.rb
+        binding.pry
     end
 
     def print_courses
